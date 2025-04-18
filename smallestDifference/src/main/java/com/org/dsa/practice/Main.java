7f41440772379e8f0097f2f3caea2a537305780d
Shanmugapriya.B <shanmugapriya.b@gds.ey.com>
2025-04-18 08:16:53 +0530

    #four number sum skeleton added

--- smallestDifference/src/main/java/com/org/dsa/practice/Main.java
+++ smallestDifference/src/main/java/com/org/dsa/practice/Main.java
@@ -1,17 +1,16 @@
 package com.org.dsa.practice;
 
+import com.org.dsa.practice.service.FourNumberSum;
 import com.org.dsa.practice.service.SmallestDifference;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         //smallest difference invocation
-        System.out.printf("Result" + invokeSmallestDifference());
+        System.out.printf("Smallest difference Result " + invokeSmallestDifference() + "\n");
+        //four number sum invocation
+        System.out.printf("Four Number Sum Result " + invokeFourNumberSum());
     }
 
     private static Map<Integer,Integer> invokeSmallestDifference(){
@@ -19,5 +18,12 @@ public class Main {
         int[] arrayList1 = {26,134,135,15,17};
         SmallestDifference smallestDifference = new SmallestDifference(arrayList,arrayList1);
         return smallestDifference.findSmallestDifference();
+    }
+
+    private static int[] invokeFourNumberSum(){
+        int[] arrayList = {7,6,4,-1,1,2};
+        int expectedSum = 16;
+        FourNumberSum fourNumberSum = new FourNumberSum(arrayList,expectedSum);
+        return fourNumberSum.findFourNumberSum();
     }
 }
---
+++ smallestDifference/src/main/java/com/org/dsa/practice/service/FourNumberSum.java
@@ -0,0 +1,15 @@
+package com.org.dsa.practice.service;
+
+public class FourNumberSum {
+    private int[] arrayListA;
+    private int expectedSum;
+
+    public FourNumberSum(int[] arrayListA, int expectedSum) {
+        this.arrayListA = arrayListA;
+        this.expectedSum = expectedSum;
+    }
+    public int[] findFourNumberSum() {
+        int[] result = new int[4];
+        return result;
+    }
+}
