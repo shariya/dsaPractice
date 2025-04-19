5b2f7d15d88f290f9c440c38c5010f982668c18f
Shanmugapriya.B <shanmugapriya.b@gds.ey.com>
2025-04-19 17:40:28 +0530

    #sortedSquare resolved using default sort method & maths.abs

--- dsaSolutions/src/main/java/com/org/dsa/practice/service/SortedSquareArray.java
+++ dsaSolutions/src/main/java/com/org/dsa/practice/service/SortedSquareArray.java
@@ -1,4 +1,20 @@
 package com.org.dsa.practice.service;
 
+import java.util.Arrays;
+
 public class SortedSquareArray {
+    private int[] inputArray;
+
+    public SortedSquareArray(int[] inputArray) {
+        this.inputArray = inputArray;
+    }
+
+    public int[] sortedSquares(){
+        int[] resultArray = new int[inputArray.length];
+        for(int i=0; i< inputArray.length; i++){
+            resultArray[i] = Math.abs(inputArray[i] * inputArray[i]);
+        }
+        Arrays.sort(resultArray);//n log n times default Timsort algorithm
+       return resultArray;
+    }
 }
