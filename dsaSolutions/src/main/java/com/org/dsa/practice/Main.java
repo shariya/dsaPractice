5d84d6f526685f550dc50b252cf39d79df9b653e
Shanmugapriya.B <shanmugapriya.b@gds.ey.com>
2025-05-21 19:08:15 +0530

    #spiral matrix

--- dsaSolutions/src/main/java/com/org/dsa/practice/Main.java
+++ dsaSolutions/src/main/java/com/org/dsa/practice/Main.java
@@ -42,7 +42,8 @@ public class Main {
     }
 
     private static List<Integer> invokeSpiralMatrix(){
-        int[][] inputArray =  {{1,2,3,4,5},{16,17,18,19,6},{15,24,25,20,7},{14,23,22,21,8},{13,12,11,10,9}};
+        //int[][] inputArray =  {{1,2,3,4,5},{16,17,18,19,6},{15,24,25,20,7},{14,23,22,21,8},{13,12,11,10,9}};
+        int[][] inputArray =  {{1,2,3,4,5},{16,17,18,19,6},{15,24,25,20,7},{14,23,22,21,8}};
         SpiralMatrix spiralMatrix = new SpiralMatrix(inputArray);
         return spiralMatrix.spiralMatrix();
     }
